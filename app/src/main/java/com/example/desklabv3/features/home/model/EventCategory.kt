8a48128a55586aa7c8483ed122a0c2144f36621c
Intan Saliya <intansaliya99@gmail.com>
2025-08-07 14:52:48 +0700

    delete: duplicated event related model

--- app/src/main/java/com/example/desklabv3/features/home/model/EventCategory.kt
+++
@@ -1,7 +0,0 @@
-package com.example.desklabv3.features.home.model
-
-enum class EventCategory {
-    EMPLOYEE_BENEFIT,
-    GENERAL_EVENT,
-    PEOPLE_DEVELOPMENT
-}
--- app/src/main/java/com/example/desklabv3/features/home/model/EventForm.kt
+++
@@ -1,8 +0,0 @@
-package com.example.desklabv3.features.home.model
-
-import java.util.UUID
-
-data class EventForm(
-    val id: UUID = UUID.randomUUID(),
-    val fields: List<FormField>
-)
