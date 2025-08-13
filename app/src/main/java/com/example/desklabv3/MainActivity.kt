7162bf583c5aeaa321f7dbfef234137b57b8ad98
Intan Saliya <intansaliya99@gmail.com>
2025-08-13 19:30:24 +0700

    add: toast

--- app/src/main/java/com/example/desklabv3/MainActivity.kt
+++ app/src/main/java/com/example/desklabv3/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.desklabv3
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.components.radiobutton.CustomRadioGroup
@@ -20,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
         dynamicRadioGroup = findViewById(R.id.rbTest)
         setupRadioButtons()
-
     }
 
     private fun setupRadioButtons() {
