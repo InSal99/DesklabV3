1e5e8990ed94b9abe020db6e5ea4cc69a73346e4
Intan Saliya <intansaliya99@gmail.com>
2025-08-17 03:51:42 +0700

    add: radio button delegate, checkbox delegate

--- app/src/main/java/com/example/desklabv3/MainActivity.kt
+++ app/src/main/java/com/example/desklabv3/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.desklabv3
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.components.button.CustomButton
 import com.example.components.radiobutton.CustomRadioGroup
+import com.example.components.radiobutton.CustomRadioGroupDelegate
 import com.example.components.toast.CustomToast
 import com.example.desklabv3.databinding.ActivityMainBinding
 
@@ -21,44 +18,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        dynamicRadioGroup = findViewById(R.id.rbTest)
-//        setupRadioButtons()
-
-//        val customButton = CustomButton(this).apply {
-//            setLabel("Click Me")
-//            setButtonSize(CustomButton.ButtonSize.MD)
-//            layoutParams = LinearLayout.LayoutParams(
-//                LinearLayout.LayoutParams.WRAP_CONTENT,
-//                LinearLayout.LayoutParams.WRAP_CONTENT
-//            ).apply {
-//                setMargins(16, 16, 16, 16)
-//            }
-//        }
-//
-//        val parentLayout = findViewById<LinearLayout>(R.id.main)
-//        parentLayout.addView(customButton)
+        dynamicRadioGroup = findViewById(R.id.rbTest)
+        setupRadioButtons()
     }
 
     private fun setupRadioButtons() {
-        // Example data
-        val options = listOf("Option 11111111", "Option 2", "Option 33333333", "Option 4")
+        val options = listOf("Option 1", "Option 2", "Option 3", "Option 4")
 
-        dynamicRadioGroup.setData(options) { it }
-
-        dynamicRadioGroup.setOnItemSelectedListener { position, data ->
-            Log.d("RadioButton", "Selected: $data at position $position")
-
-            CustomToast.success(this, "Selected: $data at position $position")
+        dynamicRadioGroup.setData(options) { item ->
+            item
         }
 
-        // Optional: Pre-select an item
-        dynamicRadioGroup.selectItem(0)
+        dynamicRadioGroup.setOnItemSelectedListener(object : CustomRadioGroupDelegate {
+            override fun onItemSelected(position: Int, data: Any?) {
+                CustomToast.success(this@MainActivity, "Selected: $data at position $position")
+            }
+        })
     }
-}
-
-data class UserOption(val id: Int, val name: String)
-
-// Extension function for easier usage
-fun <T> CustomRadioGroup.setSimpleData(dataList: List<T>) {
-    setData(dataList) { it.toString() }
 }
