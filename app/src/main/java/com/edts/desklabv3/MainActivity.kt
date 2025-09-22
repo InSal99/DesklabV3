e5b69574658612c96e248aeed106e0d753afb03e
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2025-09-22 14:51:17 +0700

    Fix bottomNav, component src, WindowInset

--- app/src/main/java/com/edts/desklabv3/MainActivity.kt
+++ app/src/main/java/com/edts/desklabv3/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.transition.Fade
@@ -80,7 +82,11 @@ class MainActivity : AppCompatActivity(), HeaderConfigurator {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+//        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+//            insets
+//        }
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .withPushAnimation()
