337f7a09a7829762ad5db6b04f50909ca9c1a68c
Intan Saliya <intansaliya99@gmail.com>
2025-10-13 11:16:50 +0700

    fix: button attr, button delegate

--- core/src/main/java/com/edts/components/button/Button.kt
+++ core/src/main/java/com/edts/components/button/Button.kt
@@ -475,4 +475,10 @@ class Button @JvmOverloads constructor(
         }
         return super.onTouchEvent(event)
     }
+
+    override fun performClick(): Boolean {
+        val handled = super.performClick()
+        buttonDelegate?.onClick(this)
+        return handled
+    }
 }
