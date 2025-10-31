2b1a46ec46c9c7c5aa3d4b1320571b2108b7a79d
Intan Saliya <intansaliya99@gmail.com>
2025-10-31 12:11:57 +0700

    fix: bottom tray

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.edts.components"
-version = "v0.13.0"
+version = "v0.14.0"
 afterEvaluate {
     publishing {
         publications {
@@ -14,7 +14,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.edts.components"
                 artifactId = "components"
-                version = "v0.13.0"
+                version = "v0.14.0"
 
                 pom {
                     name.set("Desklab Components Library")
