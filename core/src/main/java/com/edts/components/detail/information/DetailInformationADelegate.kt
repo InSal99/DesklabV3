fc01b0ba8e11c91e16f7c926bbed076655b047e7
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2026-01-08 10:55:02 +0700

    Desklab Improvement : Detail Information A
    Detail Information A:
    - Add Desc Icon to XML
    - Add HasDescIcon
    - Add descIcon (change src)
    - Add Delegate for Root Click and Desc Icon click
    - Add Ripple for Root Click and Desc Icon Click
    - Add Attr hasDescIcon and descIcon

--- core/src/main/java/com/edts/components/detail/information/DetailInformationADelegate.kt
+++ core/src/main/java/com/edts/components/detail/information/DetailInformationADelegate.kt
@@ -3,4 +3,6 @@ package com.edts.components.detail.information
 interface DetailInformationADelegate {
     fun onAction1Clicked(view: DetailInformationA)
     fun onAction2Clicked(view: DetailInformationA)
+    fun onDescIconClick(view: DetailInformationA)
+    fun onItemClick(view: DetailInformationA)
 }
