3c0ebf6536d0dd9e17c2493ebf58f4c1ee898e7e
Intan Saliya <intansaliya99@gmail.com>
2026-02-02 14:49:50 +0700

    fix: build gradle

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.edts.components"
-version = "v1.1.23"
+version = "v1.1.23-experimental"
 afterEvaluate {
     publishing {
         publications {
@@ -14,7 +14,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.edts.components"
                 artifactId = "components"
-                version = "v1.1.23"
+                version = "v1.1.23-experimental"
 
                 pom {
                     name.set("Desklab Components Library")
